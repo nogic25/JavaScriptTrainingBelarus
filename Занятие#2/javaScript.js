d50eaea193c4dabf24f7740ea76672b63897d15d
nogic25 <motolova@yahoo.com>
2019-02-05 07:42:26 -0500

    Занятие#2

--- Занятие#2/javaScript.js
+++ Занятие#2/javaScript.js
@@ -1,33 +1,33 @@
 // задача #2
-// не поняла как показать диапозон s ± p%
-/*var arr = [];
-for (var i=0, t=1000; i<t; i++) {
-   arr.push(Math.round(Math.random()*t));
-   result= arr.filter(function(elem){
-      return ( elem %10==0);
-   })
-   result.sort((a, b) => b - a);;
+
+var firstString = prompt("Введите первое значение: ");
+var secondString = prompt("Введите второе значение: ");
+
+function compare (a, b){
+var c=a.split ("").sort().join("");
+    d=b.split ("").sort().join("");
+  alert(c===d ? a + " and " + b + " are anagrams!":
+               a + " and " + b + " are not anagrams.");
+
 }
-console.log(result);
-*/
-// Создать массив из списка имен. Сгенерировать массив из 100 объектов типа «Человек». 
-//У каждого объекта должно быть случайным образом выбрано имя, возраст, а также метод,
-// который выводит имя и возраст данного человека. Прогнать методом обхода по этому массиву и заставить каждого человека представиться.
 
+compare ( firstString, secondString);
 
-var person = {
-  name: function scanNames (){
-  let firstName= ["Alex","Bob","Lisa","Slava"];
-  firstName.forEach(function(elem){
-  return elem;
-  })
-  }
-  };
-  let people = new Array(101).fill().map((e, i) => {
-     return {
-          age: Math.round(Math.random()*i),
-          names: this.name
-  
-     }
-  });
-  console.log (people);
+// задача #6
+
+var firstString = (prompt("Введите первое значение: "));
+
+function divisionThree (str){
+   var element = str.split("");
+   var sum= 0;
+   var count=0;
+for(var i = 0; i < element.length; i++) {
+      if (element[i] % 3 ==0){
+        sum=sum + parseInt(element[i]);
+        count ++;
+      }
+   }
+   console.log(" Sum: " + sum + " and Count: " + count)
+}
+
+divisionThree(firstString);
