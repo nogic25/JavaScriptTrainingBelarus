d7f4f2b4d5718be2b148d5a91ce02830b87549f7
nogic25 <motolova@yahoo.com>
2019-02-17 16:04:31 -0500

    homework 4

---
+++ Занятие#4/javaScript.js
@@ -0,0 +1,9 @@
+// задача #1
+var string = "rtoljyotyjutou 773-24-55 645656! 43445uikjgk 45gfgf5654 234-5678-00 4afd5tfvz";
+var reg =/(\d{3})-(\d{2})-(\d{2})/g
+var res= string.match(reg);
+console.log(res);
+
+
+
+
